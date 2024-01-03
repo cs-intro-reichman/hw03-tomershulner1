664641ffb643065da69c831baad664222546530e
tomer_shulner <tomer.shulner@innoviz-tech.com>
2024-01-03 18:26:23 +0200

    revert

--- LoanCalc.java
+++ LoanCalc.java
@@ -3,7 +3,7 @@
 */
 public class LoanCalc {
 	
-	static double epsilon = 0.00001;  // The computation tolerance (estimation error)
+	static double epsilon = 0.001;  // The computation tolerance (estimation error)
 	static int iterationCounter;    // Monitors the efficiency of the calculation
 	
     /** 
