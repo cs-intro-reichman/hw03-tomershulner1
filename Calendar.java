ecac2f5912fe5962a41d0afbdd74463dc9f30369
tomer_shulner <tomer.shulner@innoviz-tech.com>
2024-01-06 12:56:04 +0200

    final before comments

--- Calendar.java
+++ Calendar.java
@@ -1,7 +1,7 @@
 /** 
- * Prints the calendars of all the years in the 20th century.
+ * Prints the calendar of a specific year
  */
-public class Calendar1 {	
+public class Calendar {	
     // Starting the calendar on 1/1/1900
 	static int dayOfMonth = 1;   
 	static int month = 1;
@@ -9,31 +9,20 @@ public class Calendar1 {
 	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
 	static int nDaysInMonth = 31; // Number of days in January
 	
-	/** 
-	 * Prints the calendars of all the years in the 20th century. Also prints the  
-	 * number of Sundays that occured on the first day of the month during this period.
-	 */
 	public static void main(String args[]) {
-		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
-	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
-	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
+		int given_year = Integer.parseInt(args[0]);
 		String current_date = dayOfMonth + "/" + month + "/" + year;
-		int first_sundays = 0;
-	    //// Write the necessary initialization code, and replace the condition
-	    //// of the while loop with the necessary condition 
-	 	while (!current_date.equals("1/1/2000")) {
-	 		//// Write the body of the while 	
-			System.out.print("\n" + current_date);
-			if (dayOfWeek == 1) {
-				System.out.print(" Sunday");
-				if (dayOfMonth == 1) {
-					first_sundays++;
-				}
+
+	 	while (year != given_year + 1) {	
+			if (year == given_year) {
+				System.out.print("\n" + current_date);
+				if (dayOfWeek == 1) {
+					System.out.print(" Sunday");
+			}
 			}
 	 		advance();
 			current_date = dayOfMonth + "/" + month + "/" + year;
         }
-	 	System.out.println("\nDuring the 20th century, " + first_sundays + " Sundays fell on the first day of the month");
 	 }
 	
 	 // Advances the date (day, month, year) and the day-of-the-week.
